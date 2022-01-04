cf18c51e4b2a8b23fbe8d1893df2b32e97c11f1a
mohan2002 <64457034+mohan2002@users.noreply.github.com>
2022-01-04 18:17:55 +0530

    Changes

--- TCP Stream sockets/Client.py
+++ TCP Stream sockets/Client.py
@@ -1,7 +1,7 @@
 import socket
 
 clientsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect(("192.168.43.246",8081))
+clientsocket.connect(("192.168.43.146",8081))
 
 def get_text(clientsocket):
     buffer = ""
